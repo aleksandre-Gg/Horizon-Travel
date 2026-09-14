5b18cde5160177292e33a3e7698e23b3e78a5871
Alexander <aleksandre-Gg@users.noreply.github.com>
2026-09-14 13:31:26 +0400

    changes to menu nav

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const body = document.body;
   const menu = document.querySelector('.menu');
   const nav = document.querySelector('nav');
+  const closeB = document.querySelector('button.close');
 
   menu.addEventListener('click', () => {
     nav.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+  });
+  closeB.addEventListener('click', () => {
+    nav.classList.remove('active');
+    body.classList.remove('no-scroll');
   });
 });
